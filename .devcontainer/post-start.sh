55afe01dd9b12d2c3446678694f052a8ed7004c3
CoxlessSuperconductor <coxless.superconductor@gmail.com>
2025-09-24 00:38:04 +0900

    fix

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -2,10 +2,12 @@
 
 ## mise インストール（rootでインストール）
 curl https://mise.run | bash
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> /root/.bashrc
-echo 'eval "$(mise activate bash)"' >> /root/.bashrc
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.bashrc
+export PATH="$HOME/.local/bin:$PATH"
+echo 'eval "$(mise activate bash)"' >> $HOME/.bashrc
+eval "$(mise activate bash)"
 
-source /root/.bashrc
+source $HOME/.bashrc
 
 ## Trust and install with mise
 mise trust
@@ -15,15 +17,6 @@ MISE_NODE_VERIFY=0 mise install
 NODE_PATH="$(mise which node)"
 NPM_PATH="$(mise which npm)"
 export PATH="$(dirname "$NODE_PATH"):$(dirname "$NPM_PATH"):$PATH"
-
-echo "Using Node: $NODE_PATH $($NODE_PATH -v)"
-echo "Using npm:  $NPM_PATH  $($NPM_PATH -v)"
-
-# export NPM_PREFIX="$HOME/.npm-global"
-# mkdir -p "$NPM_PREFIX"
-# export PATH="$NPM_PREFIX/bin:$PATH"
-
-# npm config set prefix "$NPM_PREFIX"
 
 ## Install @aikidosec/safe-chain globally with pnpm
 npm install -g @aikidosec/safe-chain

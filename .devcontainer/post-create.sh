399888fd3ce947f54ec194f3232c804210bd89f7
CoxlessSuperconductor <coxless.superconductor@gmail.com>
2025-09-23 22:34:13 +0900

    test

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+## mise インストール（rootでインストール）
+curl https://mise.run | bash
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> /home/$USERNAME/.bashrc
 echo 'eval "$(mise activate bash)"' >> /home/$USERNAME/.bashrc
 
 source /home/$USERNAME/.bashrc
 
-# Trust and install with mise
+## Trust and install with mise
 mise trust
 mise install
 
-# Install @aikidosec/safe-chain globally with pnpm
+## Install @aikidosec/safe-chain globally with pnpm
 npm install -g @aikidosec/safe-chain
 safe-chain setup
